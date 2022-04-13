31e029415e456180fd1d2c9b9a6954c79ca50582
AishaHonzlová <honzlova.a@gmail.com>
2022-04-13 19:49:49 +0200

    Změna

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -7,7 +7,36 @@ public class HlavniProgram {
     public void main(String[] args) {
         Turtle zofka;
 
+                /*
+
         zofka = new Turtle();
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.turnRight(135);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(45);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(210);
+        zofka.turnRight(90);
+        zofka.move(150);
+
+        */
+
+        zofka = new Turtle();
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.turnRight(45);
+        zofka.move(200);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.move(50);
     }
 
 }
